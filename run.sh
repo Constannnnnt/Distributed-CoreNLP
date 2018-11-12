7ac7ec5e2b05fea7fe554298610d29a3ec09e179
Ji Xin <xinji1996@gmail.com>
2018-11-12 18:00:28 -0500

    OOM errors still exist

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # mvn clean package
 # spark-submit --class ca.uwaterloo.cs651.project.CoreNLP target/project-1.0.jar -input sampledata
-spark-submit --class ca.uwaterloo.cs651.project.SimpleNLP target/project-1.0.jar -input simpledata -output output -functionality ner
+spark-submit --class ca.uwaterloo.cs651.project.CoreNLP target/project-1.0.jar -input simpledata -output output -functionality ner
