a7620fe4fc92993056128eab2977d1736a524185
Kaisong <huangkaisong@126.com>
2018-11-13 17:44:16 -0500

    create hdfs files

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 # mvn clean package
 # spark-submit --class ca.uwaterloo.cs651.project.SimpleNLP --driver-memory 4G --executor-memory 4G target/project-1.0.jar -input simpledata -output output -functionality dcoref
+# hdfs dfs -mkdir -p /user/k86huang/cs651
+# hdfs dfs -put ./simpledata /user/k86huang/cs651
 spark-submit --class ca.uwaterloo.cs651.project.CoreNLP --driver-memory 4G --executor-memory 4G target/project-1.0.jar -input simpledata -output output -functionality dcoref
